fbce666ef6974e137349621ec733ac022bc0549c
edercassianoteixeira <edercassiano12015@gmail.com>
2024-06-30 18:54:58 -0300

    aula 11

---
+++ aulas/aula11/teste003.py
@@ -0,0 +1,2 @@
+nome = 'Éder'
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m'))
---
+++ aulas/aula11/teste004.py
@@ -0,0 +1,6 @@
+nome = 'Éder'
+cores = {'limpa': '\033[m',
+         'azul': '\033[34m',
+         'amarelo': '\033[33m',
+         'pretoebranco': '\033[30;107m'}
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format(cores['pretoebranco'], nome, cores['limpa']))
